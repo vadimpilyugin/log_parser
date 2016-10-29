8148d519f779823332189e8abe31b5ee872a6955
vadimpilyugin <vadimpilyugin@gmail.com>
2016-10-29 13:37:18 +0300

    working on sshd

--- regex.rb
+++ regex.rb
@@ -1,13 +1,19 @@
-class Table
-	def initialize
-		@table = []
-	end
+class Record < Hash
 public
 	def add(hsh)
-		@table << {service: hsh[:service], field: hsh[:field], value: hsh[:value], id: hsh[:id]}
+		self.update(hsh) {|k,o,n| n}
 	end
-	def size
-		@table.size
+end
+class Table < Array
+public
+	def add(hsh)
+		self << {id: hsh[:id], service: hsh[:service], data: hsh[:data], meta: hsh[:meta]}
+	end
+end
+class MatchData
+public
+	def to_h
+		self.names.zip(self.captures).to_h
 	end
 end
 
@@ -15,37 +21,33 @@ success = 0
 cnt = 0
 table = Table.new
 IO.readlines("./logs/auth.log").each{ |s|
-	if s =~ /^[\w]+\s+\d+\s+\d{2}:\d{2}:\d{2}\s+(?<server>\w+)\s+(?<service>[\w-]+)(?<pid>\[\d+\])?:\s+(?<msg>.*)?/
-		# puts "Server = #{$~[:server]}, process = #{$~[:process]}, pid = #{$~[:pid]}, msg = #{$~[:msg]}"
+	s =~ /^[\w]+\s+\d+\s+\d{2}:\d{2}:\d{2}\s+(?<server>\w+)\s+(?<service>[\w-]+)(?<pid>\[\d+\])?:\s+(?<msg>.*)?/
+	# puts "Server = #{$~[:server]}, process = #{$~[:process]}, pid = #{$~[:pid]}, msg = #{$~[:msg]}"
+	hsh = $~
+	msg = $~[:msg]
+	ip_addr = /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/
+	data = Record.new
+	meta = Record.new
+	if hsh[:service] == "sshd"
+		cnt += 1
 		success += 1
-		hsh = $~
-		msg = $~[:msg]
-		ip_addr = /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/
-		if hsh[:service] == "sshd"
-			if msg =~ /Connection from (#{ip_addr}) port (\d+) on (#{ip_addr}) port (\d+)/
-				table.add(service: hsh[:service], field: "user_ip", value: $1, id: cnt)
-				table.add(service: hsh[:service], field: "server_ip", value: $3, id: cnt)
-				table.add(service: hsh[:service], field: "user_port", value: $2, id: cnt)
-				table.add(service: hsh[:service], field: "server_port", value: $4, id: cnt)
-				table.add(service: hsh[:service], field: "meta-inf", value: {type: "new connection"}, id: cnt)
-			elsif msg =~ /Connection closed by (#{ip_addr}) [preauth]/
-				table.add(service: hsh[:service], field: "user_ip", value: $1, id: cnt)
-				table.add(service: hsh[:service], field: "meta-inf", value: {type: "closed connection", attr1: "preauth"}, id: cnt)
-			elsif msg =~ /Received disconnect from (#{ip_addr}): \d+: disconnected by (\w+)/
-				table.add(service: hsh[:service], field: "user_ip", value: $1, id: cnt)
-				table.add(service: hsh[:service], field: "username", value: $2, id: cnt)
-				table.add(service: hsh[:service], field: "meta-inf", value: {type: "disconnect"}, id: cnt)
-			else
-				puts "msg = #{hsh[:msg]}"
-				success -= 1
-			end
+		if msg =~ /Connection from (?<user-ip>#{ip_addr}) port (?<user-port>\d+) on (?<server-ip>#{ip_addr}) port (?<server-port>\d+)/
+			data = $~.to_h
+			meta = {type: "New connection"}
+		elsif msg =~ /Connection closed by (?<user-ip>#{ip_addr}) [preauth]/
+			data = $~.to_h
+			meta = {type: "Closed connection", attr1: "preauth"}
+		elsif msg =~ /Received disconnect from (?<user-ip>#{ip_addr}): \d+: disconnected by (?<username>\w+)/
+			data = $~.to_h
+			meta = {type: "Disconnect"}
 		else
+			#puts "msg = #{msg}"
 			success -= 1
+			data = {"msg" => msg}
+			meta = {"type" => "unidentified"}
 		end
-	else
-		puts s
+		table.add(id: cnt, service: hsh[:service], data: data, meta: meta)
 	end
-	cnt += 1
 }
 puts "Success #{success}/#{cnt}"
 puts "Table = #{table.size}"
